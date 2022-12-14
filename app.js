0bf087229c2bf4aa9544755ced92f17a05f5928f
Nishtan <patilnishant0311@gmail.com>
2022-12-14 23:21:22 +0530

    Payment Gateway added

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ const student = require("./models/student");
 const multer = require("multer")
 const { storage } = require("./cloudinary")
 const upload = multer({ storage })
-const secret = process.env.SECRET || "thisshouldbesecret"
+const secret = process.env.SECRET || "thisshouldbesecret";
+const checksum_lib = require("./paytm/checksum");
+const config = require("./paytm/config");
+const parseUrl = express.urlencoded({ extended: false });
+const parseJson = express.json({ extended: false });
 // const {isLoggedIn} = require('./middleware')
 
 
@@ -81,7 +85,10 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.render("home");
 })
-
+app.get('/student/research', (req, res) => {
+    res.render("college/research");
+    
+})
 //College Auth Start
 app.get('/college/register', (req, res) => {
     res.render("college/register");
@@ -120,8 +127,8 @@ app.get('/student/register', (req, res) => {
 
 app.post('/student/register', async (req, res) => {
     try {
-        const { username, email, password,college,bio  } = req.body
-        const student = new Student({ username, email,college,bio })
+        const { username, email, password, college, bio } = req.body
+        const student = new Student({ username, email, college, bio })
         const registeredStudent = await Student.register(student, password)
         res.redirect("/student/login")
     }
@@ -155,16 +162,129 @@ app.get('/student', async (req, res) => {
     res.render("lab/index", { labs });
 })
 
+app.get('/gateway', async (req, res) => {
+    res.render("payment",{currentstudent});
+})
 app.post('/book', async (req, res) => {
     const foundstudent = await student.findById(currentstudent._id)
     foundstudent.booking.push(req.body.id)
-    const bookedSlot=await Slot.findById(req.body.id)
+    const bookedSlot = await Slot.findById(req.body.id)
     bookedSlot.students.push(currentstudent._id)
-    await bookedSlot.save()
-    await foundstudent.save()
-    console.log(bookedSlot);
-    res.redirect("/student/bookings")
+    // await bookedSlot.save()
+    // await foundstudent.save()
+    res.redirect("/gateway")
 })
+app.post("/paynow", [parseUrl, parseJson], (req, res) => {
+    // Route for making payment
+
+    var paymentDetails = {
+        amount: req.body.amount,
+        customerId: req.body.name,
+        customerEmail: req.body.email,
+        customerPhone: req.body.phone
+    }
+    if (!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail || !paymentDetails.customerPhone) {
+        res.status(400).send('Payment failed')
+    } else {
+        var params = {};
+        params['MID'] = config.PaytmConfig.mid;
+        params['WEBSITE'] = config.PaytmConfig.website;
+        params['CHANNEL_ID'] = 'WEB';
+        params['INDUSTRY_TYPE_ID'] = 'Retail';
+        params['ORDER_ID'] = 'TEST_' + new Date().getTime();
+        params['CUST_ID'] = paymentDetails.customerId;
+        params['TXN_AMOUNT'] = paymentDetails.amount;
+        params['CALLBACK_URL'] = 'http://localhost:3000/callback';
+        params['EMAIL'] = paymentDetails.customerEmail;
+        params['MOBILE_NO'] = paymentDetails.customerPhone;
+
+
+        checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
+            var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
+            // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+
+            var form_fields = "";
+            for (var x in params) {
+                form_fields += "<input type='hidden' name='" + x + "' value='" + params[x] + "' >";
+            }
+            form_fields += "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "' >";
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' + txn_url + '" name="f1">' + form_fields + '</form><script type="text/javascript">document.f1.submit();</script></body></html>');
+            res.end();
+        });
+    }
+});
+app.post("/callback", (req, res) => {
+    // Route for verifiying payment
+
+    var body = '';
+
+    req.on('data', function (data) {
+        body += data;
+    });
+
+    req.on('end', function () {
+        var html = "";
+        var post_data = qs.parse(body);
+
+        // received params in callback
+        console.log('Callback Response: ', post_data, "\n");
+
+
+        // verify the checksum
+        var checksumhash = post_data.CHECKSUMHASH;
+        // delete post_data.CHECKSUMHASH;
+        var result = checksum_lib.verifychecksum(post_data, config.PaytmConfig.key, checksumhash);
+        console.log("Checksum Result => ", result, "\n");
+
+
+        // Send Server-to-Server request to verify Order Status
+        var params = { "MID": config.PaytmConfig.mid, "ORDERID": post_data.ORDERID };
+
+        checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
+
+            params.CHECKSUMHASH = checksum;
+            post_data = 'JsonData=' + JSON.stringify(params);
+
+            var options = {
+                hostname: 'securegw-stage.paytm.in', // for staging
+                // hostname: 'securegw.paytm.in', // for production
+                port: 443,
+                path: '/merchant-status/getTxnStatus',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': post_data.length
+                }
+            };
+
+
+            // Set up the request
+            var response = "";
+            var post_req = https.request(options, function (post_res) {
+                post_res.on('data', function (chunk) {
+                    response += chunk;
+                });
+
+                post_res.on('end', function () {
+                    console.log('S2S Response: ', response, "\n");
+
+                    var _result = JSON.parse(response);
+                    if (_result.STATUS == 'TXN_SUCCESS') {
+                        res.send('payment sucess')
+                    } else {
+                        res.send('payment failed')
+                    }
+                });
+            });
+
+            // post the data
+            post_req.write(post_data);
+            post_req.end();
+        });
+    });
+});
 
 app.get('/student/bookings', async (req, res) => {
     const student = await Student.findById(currentstudent._id).populate("booking")
@@ -196,12 +316,13 @@ app.post('/lab', upload.single('images'), async (req, res) => {
             temp.end = e.end;
             temp.lab = lab._id
             temp.date = String(tempdate.getDate()) + '-' + String(tempdate.getMonth()) + '-' + String(tempdate.getFullYear());
-            lab.slots.push(temp._id)
-            await temp.save()
+            lab.slots.push(temp._id);
+            await temp.save();
         })
         tempdate = new Date(tempdate.getTime() + 1000 * 60 * 60 * 24)
     }
     lab.url = req.file.path
+    college.labs.push(lab);
     await lab.save()
     await college.save()
     res.redirect("/lab")
---
+++ paytm/config.js
@@ -0,0 +1,6 @@
+var PaytmConfig = {
+    mid: "DEgVJT89791848957870",
+    key: "XgGHn8dV%snqr_mi",
+    website: "WEBSTAGING",
+  };
+module.exports.PaytmConfig = PaytmConfig;
